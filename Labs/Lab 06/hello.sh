b0e2f42051915b61ab2ea09071fd33a07b8a8dfc
Vivian Do <odnaiviv@hotmail.com>
2021-11-06 14:48:20 -0400

    Add files via upload

---
+++ Labs/Lab 06/hello.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Step 1
+echo "Welcome to computer science society."
+
+# Step 2
+# Name: Vivian Do - Email: vdo10@student.gsu.edu
+
+# Step 3
+echo "Date is = `date`"
+
+# Step 4
+echo */ | wc
+
+# Step 5
+echo $PATH
+echo $USER
+echo $SHELL
+
+# Step 6
+echo "Printing disk usage"
+echo "`df`"
+
+# Step 7
+echo "Please, could you loan me "$"25.00?"
+
+# Step 8
+echo "if x = 2, x*x = 4, x/2 = 1"
+
+# Step 9
+echo "`ls *.sh | grep c`"
+
+# Step 10
+echo "Good Bye"
+echo "Current Hour = `date +"%H"`"
+
+# End of hello.sh script
