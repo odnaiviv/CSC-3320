3d1dd9ce179408db161e0656cc872ad6d359d8f8
Vivian Do <odnaiviv@hotmail.com>
2021-11-06 15:01:18 -0400

    Add files via upload

---
+++ Labs/Lab 08/q2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+	int letters;
+	int words;
+	char character;
+	int space = 0;
+
+	printf("Enter a sentence: ");
+
+	while ((character = getchar()) != '\n') {
+		if (character != ' ') {
+			if(!space) {
+				words++;
+				space = 1;
+			}
+			letters++;
+		} else
+			space = 0;
+	}
+
+	printf("Average word length: %.1f", (float)letters/words);
+
+	return 0;
+}
